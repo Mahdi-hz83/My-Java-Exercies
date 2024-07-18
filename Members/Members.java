bf8843c609939d5703a8b787262c1ed6c57a8a95
Mahdi <mahdihosseinzadeh.uut1401@gmail.com>
2024-07-18 11:11:29 +0330

    Intializing Abstract Memebrs Code

--- Members/Members.java
+++ Members/Members.java
@@ -1,4 +1,40 @@
 package org.example.Members;
 
-public class Members {
+import org.example.Books.Books;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Members {
+    protected String name;
+    protected String memberId;
+    protected List<Books> borrowedBooks;
+
+    public Members(String name , String memberId){
+        this.name = name;
+        this.memberId = memberId;
+        borrowedBooks = new ArrayList<>();
+    }
+
+    public abstract int getBorrowDuration();
+
+    public void borrowBook(Books book){
+        if (book.isAvailable()){
+            borrowedBooks.add(book);
+            book.setAvailable(false);
+        }
+        else{throw new IllegalArgumentException("Book is not available");}
+    }
+    public void returnBook(Books book){
+        if (borrowedBooks.remove(book)){
+            book.setAvailable(true);
+        }
+        else{throw new IllegalArgumentException("This book was not borrowed by this member.");}
+    }
+
+    @Override
+    public String toString() {
+        return "Members [Name=" + name + ", ID=" + memberId + "]";
+    }
+
 }
